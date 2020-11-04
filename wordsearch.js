cdbb1eb355ee42d282013031c5358eaab0c3a838
Matt <matt.hd.ford@gmail.com>
2020-11-04 22:22:07 +0000

    adding wordsearch, completed

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,27 @@
-const wordSearch = (letters, word) => { 
+const wordSearch = (letters, word) => {
     const horizontalJoin = letters.map(ls => ls.join(''))
+    console.log(horizontalJoin);
     for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+        // console.log(`l of horizontalJoin is: ${l}`)
+        if (l.includes(word)) {
+            return true;
+        }
+        // map function to transpose vertical array into horizontal array. 
+        // iterate through the transposed vertical array, similar to the first step 
+        const verticalArray = letters[0].map((col, index) => letters.map((row, jndex) => row[index]))
+        console.log(verticalArray);
+        const verticalJoin = verticalArray.map(index => index.join(''));
+        for (l of verticalJoin) {
+            console.log(`l of verticalJoin is: ${l}`)
+            if (l.includes(word)) {
+                return true;
+            }
+        };
     }
+    return false;
 }
 
+
+
 module.exports = wordSearch
+
